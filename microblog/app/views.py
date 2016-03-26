2a7fa95762eddd2a4dc34ac1d9f949803754da62
pultitom <t.pultinevicius@gmail.com>
2016-03-26 14:44:00 +0000

    base template, loops in templates

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -5,10 +5,30 @@ from app import app
 @app.route("/index")
 def index():
     user = { 'nickname' : 'Tomas' } #fake user
+    posts = [
+        {
+            'author': { 'nickname' : 'Jonas' },
+            'body' : 'Beautiful day in Vilnius'
+        },
+        {
+            'author' : { 'nickname' : 'Susan' },
+            'body' : 'The Avengers movie is cool. Except capt. America character of course'
+        }
+    ]
+
     app.logger.debug("index page requested")
     return render_template('index.html',
                             title = 'Home',
-                            user = user)
+                            user = user,
+                            posts = posts)
+
+
+@app.route("/new")
+def new():
+    app.logger.debug("new page requestet")
+    return render_template("index.html",
+            user = { 'nickname' : 'Tomas' })
+
 
 @app.errorhandler(404)
 def page_not_found(e):
